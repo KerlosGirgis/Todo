6dd1021793d3fd9de8757b84a6a0858edd8c951d
kerlos <kerlos0@protonmail.com>
2024-12-13 16:23:45 +0200

    fixed a bug in verse of the day

--- lib/services/verse_manager.dart
+++ lib/services/verse_manager.dart
@@ -1,16 +1,33 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class VerseManager {
   static List<String>? _verses;
+  static String installId="";
 
   // Load and cache verses
   static Future<void> loadVerses() async {
     if (_verses == null) {
       String content = await rootBundle.loadString('assets/nasb.json');
       _verses = List<String>.from(json.decode(content));
+    }
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tempInstallId = prefs.getString('installId');
+    if (tempInstallId == null) {
+      // Generate a new random install-specific identifier
+      tempInstallId = Random().nextInt(1000000).toString();
+      await prefs.setString('installId', tempInstallId).then((onValue){
+        installId=tempInstallId!;
+      });
+    }
+    else{
+      installId=tempInstallId;
     }
   }
 
@@ -22,7 +39,7 @@ class VerseManager {
 
     // Use today's date to create a consistent random seed
     String today = DateFormat('yyyy-MM-dd').format(DateTime.now());
-    int seed = today.hashCode;
+    int seed = today.hashCode+installId.toString().hashCode;
     Random random = Random(seed);
 
     // Pick a random verse based on the seed
